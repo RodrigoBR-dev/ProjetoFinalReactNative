395172a1161906ca0f98ded53ab54e4cc462f21b
Mateus Satoh <mateussatoh@gmail.com>
2021-07-14 09:05:29 -0300

    add: product details basic styles

--- src/Screens/ProductDetails/index.jsx
+++ src/Screens/ProductDetails/index.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 
-import { View, Text } from "react-native";
-
+import { View, Image, Text } from "react-native";
+import { Feather, AntDesign } from "react-native-vector-icons";
 import { styles } from "./styles";
 
 import Header from "../../components/Header";
+import money from "../../util/money";
 
 export default function ProductDetails({ route }) {
   const { name, price, image, description } = route.params;
   return (
-    <View style={styles.container}>
+    <>
       <Header isDetailsPage />
-      <Text>{name}</Text>
-    </View>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: image }} />
+
+        <View style={styles.detailsContainer}>
+          <View>
+            <Text style={styles.title}>Descrição</Text>
+            <Text style={styles.paragraph}>{description}</Text>
+          </View>
+          <View>
+            <Text style={styles.pricing}>{money.formatarParaReal(price)}</Text>
+            <View style={styles.iconContainer}>
+              <AntDesign name="barcode" size={30} style={styles.icon} />
+              <Text style={styles.paragraph}>
+                {money.formatarParaReal(parseInt((price * 9) / 10))} no boleto
+              </Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Feather name="credit-card" size={30} style={styles.icon} />
+              <Text style={styles.paragraph}>
+                12X de {money.formatarParaReal(parseInt(price / 10))}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
--- src/Screens/ProductDetails/styles.js
+++ src/Screens/ProductDetails/styles.js
@@ -2,6 +2,35 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginTop: 30,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 22,
+  },
+  paragraph: {
+    fontSize: 14,
+  },
+  pricing: {
+    fontSize: 26,
+  },
+  iconContainer: {
+    flexDirection: "row",
+  },
+  icon: {
+    marginRight: 10,
   },
 });
