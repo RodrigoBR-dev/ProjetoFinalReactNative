f40b34295dcec9a2e5d68fcba9e387f6d6f266a8
paulaLopes2270 <plnhschltz@gmail.com>
2021-07-15 22:09:56 -0300

    pagina favoritos

--- src/Screens/Favorite/index.js
+++ src/Screens/Favorite/index.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import asyncStorage from "../../service/asyncStorage";
 import styles from "./styles";
+import apiProduto from "../../service/apiProduto";
+import Card from "../../components/card";
+import Produto from "../../model/produto";
 
 const Favorite = () => {
   const [product, setProduct] = useState([]);
+  const [nameProduct, setNameProduct] = useState("");
   useEffect(() => {
-    store2()
-
+    store()
+    renderFavorite()
   }, []);
-
-  const store2 = async() => {
-    const store = await asyncStorage.getFavorite();
-    console.log(store)
+  
+  const store = async() => {
+    setNameProduct(await asyncStorage.getFavorite());
+    
   }
+  
+  const renderFavorite = async () => {
+    console.log(nameProduct)
+    const resposta = await apiProduto.searchProductByName(nameProduct)
+    setProduct(resposta.data.map((produto) => new 
+    Produto(produto)));
+    // console.log(product)
+  };
+
+  const renderProduto = ({ item }) => (
+    <Card
+      name={item.nome}
+      price={item.preco}
+      image={item.url}
+      description={item.descricao}
+    />
+  );
  
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Sou um favotiro</Text>
-    </View>
+    
+  <FlatList
+    data={product}
+    keyExtractor={(item) => item.nome}
+    numColumns={2}
+    renderItem={renderProduto}
+    contentContainerStyle={styles.container}
+  />
   );
-};
-
+  }
 export default Favorite;
